6e5d3fc7b537d9370b866b3a711402a09cacbeda
Julia <julkuleshova@gmail.com>
2024-04-08 00:15:31 +0500

    fix modal accessibility

--- scripts.js
+++ scripts.js
@@ -14,12 +14,12 @@ window.addEventListener("DOMContentLoaded", () => {
     const inputBtnDecrease = input.querySelector('.input-quantity__btn[data-action=decrease]');
     inputBtnIncrease.addEventListener('click', () => {
       const initialValue = inputField.value * 1;
-      inputField.value = initialValue + 1; 
+      inputField.value = initialValue + 1;
     });
     inputBtnDecrease.addEventListener('click', () => {
       const initialValue = inputField.value * 1;
       if (initialValue > 1) inputField.value = initialValue - 1;
-    });    
+    });
   });
 
   const accordions = document.querySelectorAll('.accordion__item');
@@ -31,29 +31,62 @@ window.addEventListener("DOMContentLoaded", () => {
       if (accordion.classList.contains('accordion__item_active')) {
         title.setAttribute('aria-expanded', 'true');
       } else {
-        title.setAttribute('aria-expanded', 'false');   
+        title.setAttribute('aria-expanded', 'false');
       }
-    });    
+    });
   });
 
   const modalTarget = document.querySelectorAll('.modal-target');
-  modalTarget.forEach((modalTarget)=> {
-    modalTarget.addEventListener('click', ()=>{
+  modalTarget.forEach((modalTarget) => {
+    modalTarget.addEventListener('click', (el) => {
       const modalWindow = document.querySelector('.modal');
+      el.target.classList.add('lastFocus');
       showModal(modalWindow);
     });
   });
-  const modalBackdrop = document.querySelectorAll('.modal-backdrop');
-  modalBackdrop.forEach((modalBackdrop)=> {
-    modalBackdrop.addEventListener('click', (e) => {
 
+  const closeModal = (modalWindow) => {
+    const lastFocusedElement = document.querySelector('.lastFocus');
+
+    modalWindow.classList.remove('show-modal');
+    setTimeout(() => {
+      lastFocusedElement.focus();
+      lastFocusedElement.classList.remove('lastFocus');
+    }, 1);
+  }
+
+  const modalBackdrop = document.querySelectorAll('.modal-backdrop');
+  modalBackdrop.forEach((modalBackdrop) => {
+    modalBackdrop.addEventListener('click', (e) => {
       const modalWindow = e.target.closest('.modal');
-      modalWindow.classList.remove('show-modal');
+      closeModal(modalWindow);
+
     });
   });
 
   const showModal = (modalWindow) => {
+    const closeButton = document.querySelector('.modal-close-button');
+
+    closeButton.addEventListener('onClick', () => {
+      closeModal(modalWindow);
+    })
+
+    modalWindow.addEventListener("keydown", (event) => {
+      const code = event.keyCode || event.which;
+
+      if (code === 9) {
+        if (closeButton === document.activeElement) {
+          event.preventDefault();
+          closeButton.focus();
+        }
+      } else if (code === 27) {
+        closeModal(modalWindow);
+      }
+    })
+
     modalWindow.classList.add('show-modal');
+    setTimeout(() => modalWindow.focus(), 1);
   }
+
 
 })
